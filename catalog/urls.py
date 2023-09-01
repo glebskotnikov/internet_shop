b7fee2deb7ac274ea0e15f373e46ccb8822f2b42
glebskotnikov <skotnikov.nsk@gmail.com>
2023-09-01 00:12:14 -0600

    3, 4 task completed

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,5 @@ from catalog import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    # Здесь вы можете добавить пути к вашим статическим файлам,
+    # если они располагаются вне приложений
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
