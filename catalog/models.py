6cc9950c7bcfe39dac6903d531fe5a1fbc1b66f3
glebskotnikov <skotnikov.nsk@gmail.com>
2023-09-05 12:30:37 -0600

    20.1_task 3 - completed

--- catalog/models.py
+++ catalog/models.py
@@ -4,8 +4,22 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    photo = models.IntegerField(upload_to='products/', **NULLABLE, verbose_name='Фото')
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    date_of_creation = models.DateField(verbose_name='Дата создания')
+    date_last_modified = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(**NULLABLE)
