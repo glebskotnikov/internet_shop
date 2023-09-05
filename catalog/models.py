3fef03e9f860f5a57e9d3d0b8fb57807220cad58
glebskotnikov <skotnikov.nsk@gmail.com>
2023-09-05 11:39:15 -0600

    20.1_task 2 - completed

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
