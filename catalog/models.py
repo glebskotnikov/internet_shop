8e6d20d962a36a9dda8f7b57490f6940ac659042
glebskotnikov <skotnikov.nsk@gmail.com>
2023-09-05 17:22:08 -0600

    20.1_task 4 - remove migration created_at

--- catalog/models.py
+++ catalog/models.py
@@ -23,7 +23,6 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Категория')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
-    created_at = models.DateField(**NULLABLE, verbose_name='Дата создания')
 
     def __str__(self):
         return f'{self.name}'
