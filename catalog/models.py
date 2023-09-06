d746337e375c893d144106d5095f234c0869ebd3
glebskotnikov <skotnikov.nsk@gmail.com>
2023-09-05 19:00:38 -0600

    20.1_task 5 - done

--- catalog/models.py
+++ catalog/models.py
@@ -3,23 +3,6 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-    photo = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Фото')
-    category = models.CharField(max_length=100, verbose_name='Категория')
-    price = models.IntegerField(verbose_name='Цена за покупку')
-    date_of_creation = models.DateField(verbose_name='Дата создания')
-    date_last_modified = models.DateField(verbose_name='Дата последнего изменения')
-
-    def __str__(self):
-        return f'{self.name} ({self.category})'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Категория')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
@@ -30,3 +13,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    photo = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Фото')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_last_modified = models.DateTimeField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
