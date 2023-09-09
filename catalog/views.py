d295dd1045c13ad016e37441d1e41bff605cbbe3
glebskotnikov <skotnikov.nsk@gmail.com>
2023-09-09 12:53:56 -0600

    20.2_task 1,2,3 - done

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    # print(Product.objects.all()[:5])
+    context = {
+        'objects_list': Product.objects.all(),
+        'title': 'Магазин - главная'
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def product(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk)
+    }
+    return render(request, 'catalog/product.html', context)
 
 
 def contacts(request):
