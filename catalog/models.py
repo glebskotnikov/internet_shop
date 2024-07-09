cee7cf17056e8bed0ff024e42c4ab0d5768f12df
glebskotnikov <skotnikov.nsk@gmail.com>
2024-07-09 13:45:30 -0700

    22.2 task 3 - done! (Add 'owner' field to Product model, updated the ProductCreationView to assign the owner as the currently logged in user. Restricted product creation access to authenticated users only.)

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 NULLABLE = {'blank': True, 'null': True}
 
 
@@ -23,6 +25,8 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена за покупку')
     date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания', **NULLABLE)
     date_last_modified = models.DateTimeField(verbose_name='Дата последнего изменения', **NULLABLE)
+
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='Владелец', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} ({self.category})'
